7cd7864a514fe9fcc188ba3e23ced554c4d9c3a2
Amruta Abhangrao <113428747+amrutaabhangrao@users.noreply.github.com>
2024-06-10 11:01:12 +0530

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,5 @@
+#Write a program that adds the digits in a 2 digit number.
+two_digit_number = input()
+first_digit = int(two_digit_number[0])
+second_digit = int(two_digit_number[1])
+print(first_digit + second_digit)
