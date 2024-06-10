bc2cafebd42983198bec6a467d456a022ccb0e13
Amruta Abhangrao <113428747+amrutaabhangrao@users.noreply.github.com>
2024-06-10 13:26:22 +0530

    Create leapyear_or_not.py

---
+++ leapyear_or_not.py
@@ -0,0 +1,13 @@
+# Leap year or not
+year = int(input())
+ 
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year")
+    else:
+      print("Not leap year")
+  else:
+     print("Leap year")
+else:
+   print("Not leap year")
